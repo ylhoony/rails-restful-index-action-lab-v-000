6313017944b7122265cbec6c717b442507e5aaee
Hoon Lee <yl.hoony@gmail.com>
2018-01-18 20:45:14 +0000

    Done.

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,8 @@
+class StudentsController < ApplicationController
+
+  def index
+    @students = Student.all
+    render "students/index"
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  
+
+  get "/students" => "students#index"
+
 end
